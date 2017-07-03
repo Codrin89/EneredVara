ead39271722f3609827c4e29a0450eb104e99e2a
Web6 <Web6>
2017-07-03 22:00:25 +0300

    Curs7

---
+++ Curs9/index.js
@@ -0,0 +1,84 @@
+(function() {
+
+	var z = 10;
+	console.log(z);
+	z=z+5;
+	console.log(z);
+	z = z + 'mere!';
+	console.log(z);
+
+	var w = '10'/ 2;
+	console.log (w);
+
+	var array = ['one', 'two', 'three', 'seven'];
+	console.log(array);
+	console.log(array[0]);
+
+	var object = {
+		'object1': 12345213,
+		'object2': 'something',
+		'object3': 'Ana are mere'
+}
+	console.log(object);
+	console.log(object.object3);
+
+	var boolean1 = true;
+	var boolean2 = false;
+	console.log(boolean1);
+	console.log(boolean2)
+
+	alert('something');
+	prompt('what is your name');
+
+	
+	if ( 1 == 1 ) {
+		console.log('Adevarat');
+	} else{
+		console.log('False');
+	}
+
+	var z = 2;
+	while( z < 10) {
+		console.log(z);
+		z = z + 2;
+	}
+
+	for (var i = 0 ; i < 5 ; i++) {
+		console.log(i);
+	}
+
+	var condition = 'Elena';
+	switch(condition) {
+		case 'Eva' :
+			console.log('Eva');
+			break;
+		case 'Andreea' :
+			console.log('Andreea');
+		case 'Bogdan' :
+			console.log('Bogdan');
+		default :
+			console.log('default');
+			break;
+	}
+
+	var input = document.getElementById('username');
+	console.log(input);
+	var input2 = document.getElementsByClassName('usernameClass');
+	console.log(input2);
+
+	document.getElementById('btn-submit').addEventListener('click', function() {
+		alert('I have clicked the button');
+		var input = document.getElementById('username');
+		console.log(input.value);
+		var input2 = document.getElementById('username2');
+		console.log(input2.value);
+		if(input2.value.length < 8) {
+			alert('password is too short');
+		// }else{
+		// 	alert('password is long enough');
+		// }
+
+	});
+
+
+})();
