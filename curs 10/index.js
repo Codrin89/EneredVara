717b794ee52ce90cc3859e30cbb10595c6f14fc3
andreeabodescu <bodescu_andreea@yahoo.com>
2017-07-22 19:11:34 +0300

    curs 10

---
+++ curs 10/index.js
@@ -0,0 +1,14 @@
+(function() {
+
+	$('#openModal').on('click', function() {
+		$('#modalTest').modal();
+	});
+	$('button[data-toggle="tooltip"]').tooltip();
+	$('[data-toggle="popover"]').popover();
+	$('.carousel').carousel({
+		interval: 800,
+		pause: 'hover'
+	})
+
+})();
+
