817e263a57d8e35450d1ae0f98302f8c7d09fa6d
Web6 <Web6>
2017-07-22 19:11:15 +0300

    curs10

---
+++ Curs 10/index.js
@@ -0,0 +1,13 @@
+(function () {
+	$('#openModel').on('click', function() {
+		$('#modalTest').modal();
+	});
+	$('button[data-toggle="tooltip"]').tooltip()
+	$('button[data-toggle="popover"]').popover()
+	$('.carousel').carousel({
+		interval:800,
+		pause: 'hover'
+	})
+})();
+
+
