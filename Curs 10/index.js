9d55e4a8547a481627f2aee811b4609cee7803cb
BeniaminParau <beniamin_parau@yahoo.com>
2017-07-22 19:01:03 +0300

    Curs 10

---
+++ Curs 10/index.js
@@ -0,0 +1,13 @@
+(function() {
+
+	$('#openModal').on('click', function() { 
+		$('#modalTest').modal();
+	});	
+	$('[data-toggle="tooltip"]').tooltip()
+	$('[data-toggle="popover"]').popover()
+	
+    $('.carousel').carousel({
+        interval: 100,
+        pause: 'hover'
+     })   
+})();
---
+++ Curs 10/index3.js
@@ -0,0 +1,20 @@
+(function() {
+    var numberArray = [];
+    document.getElementById('add').addEventListener('click', function() {
+    	numberArray.push(parseInt(document.getElementsByClassName('text-operator')[0].value));
+    	console.log(numberArray);
+    	document.getElementsByClassName('text-operator')[0].value = '';
+    });
+
+
+    document.getElementById('calculateMa').addEventListener('click', function () {
+    	var ma = 0;
+    	for(var i = 0 ; i < numberArray.length ; i++) {
+    		ma += numberArray[i];
+    	}
+    	ma = ma/numberArray.length;
+    	document.getElementById('result').innerHTML = ma;
+    });
+
+
+})();
