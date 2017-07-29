d2371e7399be6286a31548a00f982808647b4952
Wydder <bogdan.mosica@gmail.com>
2017-07-30 01:30:28 +0300

    Curs12

--- Curs 12/index.js
+++ Curs 12/index.js
@@ -1,5 +1,4 @@
-(
-	function() {
+(function() {
 		//select all hover boxes from page
 		//boxesHover is an array
 		var boxesHover = document.getElementsByClassName('hover-image')
@@ -7,6 +6,16 @@
 		for(var i = 0 ; i < boxesHover.length ; i ++) {
 			boxesHover[i].addEventListener('click', function() {
 				alert('Am dat click pe item')
+			})
+		}		
+	}		
+);
+(function() {
+		var downloadButton = document.getElementsByClassName('downloadSlide')
+		var downloadBox = document.getElementsByClassName('downloadSlidesBox')
+		for(var i = 0 ; i < downloadButton.length ; i ++) {
+			downloadButton[i].addEventListener('click', function() {
+				downloadBox.style.display = "block";
 			})
 		}
 	}
