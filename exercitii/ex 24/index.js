3f51092436b2d39c589179cd83daeb4fe4775422
Web6 <Web6>
2017-10-07 13:46:29 +0300

    Ex 24,25,26

---
+++ exercitii/ex 24/index.js
@@ -0,0 +1,42 @@
+function isAnagram() {
+	//check is word are anagrams
+
+	var firstWord = document.getElementById('firstWord').value;
+	var secondWord = document.getElementById('secondWord').value;
+	console.log(firstWord);
+	console.log(secondWord);
+	firstWord = firstWord.split('');
+	secondWord = secondWord.split('');
+	console.log(firstWord);
+	console.log(secondWord);
+
+	if (firstWord.length != secondWord) {
+		alert('not an anagram');
+			return;
+	};
+	for (var i = 0 ; i < firstWord.length ; i++) {
+		var letterCheckBoolean = false;
+		for(var j = 0 ; j < secondWord.length ; j++) {
+			if(firstWord[i] == secondWord[j] && letterCheckBoolean != true) {
+				//am gasit litera
+				var letterCheckBoolean = true;
+				secondWord.splice(j, 1);
+				j--;
+			}
+
+		}
+		if(letterCheckBoolean == false) {
+			alert('not an anagram');
+			return;
+		}
+	}
+	alert('words are anagrams!!!');
+}
+
+
+
+document.getElementByTagName('body')[0].addEventListener('keydown', function(e))
+	if (event.keyCode == 13) {
+		alert('you pressed enter');
+	}
+})
---
+++ exercitii/ex 26/index.js
@@ -0,0 +1,7 @@
+(function (){
+
+
+n=(1/30)*(Math.log(1+(b/p)*(1-Math.pow(1+i),30)))/Math.log(1+i))
+
+})
+
