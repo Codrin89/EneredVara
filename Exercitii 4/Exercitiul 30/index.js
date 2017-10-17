0883dbc8f1bd4663f50fe6a5a1abf4140aac9edd
BeniaminParau <beniamin_parau@yahoo.com>
2017-10-17 21:53:54 +0300

    Curs 25

--- Exercitii 4/Exercitiul 30/index.js
+++ Exercitii 4/Exercitiul 30/index.js
@@ -1,27 +1,29 @@
 (function() {
-     
-     var number = 12;
-     //<div>
-         // <span class="number"></span>
-     // </div>    
-     function geberateMultiTable () {
-     	var divItem = $('<div></div>');
-     	divItem.append($('<span class="number"> </span>'));
-     	for(var i = 0 ; i < number; i++) {
-     		var numberItem = $('<span class="number">' + i + '</span>');
-     		divItem.append(numberItem);
-     	}
-     	$('.col-md-12').append(divItem);
-     }
-     $('.col-md-12').append(divItem);
 
-     
+    var number = 12;
+    //<div>
+        // <span class="number"></span>
+    // </div>
+    function generateMultiTable() {
+        var divItem = $('<div></div>');
+        divItem.append($('<span class="number"> </span>'));
+        for(var i = 0 ; i <= number; i++) {
+            var numberItem = $('<span class="number">' + i + '</span>');
+            divItem.append(numberItem);
+        }
+        $('.col-md-12').append(divItem);
 
+        for(var i = 0 ; i <= number; i++) {
+            var divItem = $('<div></div>');
+            divItem.append($('<span class="number">'+ i +'</span>'));
+            for( var j = 0 ; j <= number ; j++) {
+                var numberItem = $('<span class="number">' + (i*j) + '</span>');
+                divItem.append(numberItem);
+            }
+            $('.col-md-12').append(divItem);
+        }
 
-     
+    }
 
-
-
-
-    generateMultiTable;  
+    generateMultiTable();
 })();
